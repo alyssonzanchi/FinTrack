7c6cfe8c20b0987c30c0ff9eb269f9e4122c3856
Alysson Zanchi <alyssonzanchi@gmail.com>
2024-11-19 09:53:30 -0300

    feat: added register and login methods

--- src/main/java/br/edu/ifrs/fintrack/exception/DatabaseIntegrityException.java
+++
@@ -1,7 +0,0 @@
-package br.edu.ifrs.fintrack.exception;
-
-public class DatabaseIntegrityException extends RuntimeException {
-    public DatabaseIntegrityException(String message) {
-        super(message);
-    }
-}
